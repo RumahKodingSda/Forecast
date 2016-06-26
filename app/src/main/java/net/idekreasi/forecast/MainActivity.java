0e2dd842cb1817b95ef50e2a6cc9281a41a989c1
Uddin <saifuddinmutammam@gmail.com>
2016-06-26 13:57:47 +0700

    TestCommit

---
+++ app/src/main/java/net/idekreasi/forecast/MainActivity.java
@@ -0,0 +1,15 @@
+package net.idekreasi.forecast;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ikrenet on 26/06/16.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
